545930f85804c777c1bd2ce383229fc1275cc3a9
cjk12 <ckarcher@mit.edu>
2016-04-15 19:28:56 -0400

    pep8 compliance

--- gpkit/tools.py
+++ gpkit/tools.py
@@ -61,7 +61,8 @@ def te_secant(x, nterm):
                        15514534163557086905] )
     if nterm > 12:
         n_extend = np.asarray(range(13, nterm+1))
-        E2n_add = 8 * np.sqrt(n_extend/np.pi) * (4*n_extend/(np.pi * np.exp(1)))**(2*n_extend)
+        E2n_add = (8 * np.sqrt(n_extend/np.pi) 
+                      * (4*n_extend/(np.pi * np.exp(1)))**(2*n_extend))
         E2n = np.append(E2n, E2n_add)
 
     res = 1
